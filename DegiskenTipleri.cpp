9d01cce2dd3267640051a9872a6e4f4137568098
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-07 20:19:58 +0300

    Update DegiskenTipleri.cpp

--- DegiskenTipleri.cpp
+++ DegiskenTipleri.cpp
@@ -1,1 +1,25 @@
+//DEĞİŞKEN TİPLERİ
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int a; //integer (tamsayı) değişken tipi
+    a = 21;
+    cout << a << endl;
+    char b = 'x';
+    cout << b << endl;
+    float e = 2.72; //float-double-long double (ondalık sayılar için)
+    cout << e << endl;
+    long double y = 99.8734;
+    cout << y << endl;
+    bool yas = true;  //boolean (true-false)
+    
+    bool gun = false;
+    
+    cout << yas << endl;
+    cout << gun << endl;
+    
+    return 0;
+}
