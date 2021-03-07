32c1caed6298179210636e16420e31ba4342a9f7
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-07 21:04:32 +0300

    Update TipDonusumleri.cpp

--- TipDonusumleri.cpp
+++ TipDonusumleri.cpp
@@ -1,1 +1,26 @@
+//TİP DÖNÜŞÜMLERİ
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 100; 
+    
+    cout << a << endl;
+
+    cout << (char)a << endl;
+    char b = 'm';
+     
+    
+    cout << b << endl;
+    
+    cout << (int)b << endl;
+    float m = 13.15;
+    
+    
+    cout << m << endl;
+    
+    cout << (int)m << endl;
+    
+    return 0;
+}
