baa87027a191acf3ddcaf680747e7235ef412da3
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-08 15:38:12 +0300

    Create TemelGirisCikisIslemleri.cpp

---
+++ TemelGirisCikisIslemleri.cpp
@@ -0,0 +1,15 @@
+//TEMEL GİRİŞ ÇIKIŞ İŞLEMLERİ
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Bir sayi giriniz: " << endl;
+    cin >> a; //klavyeden girilecek değer
+    cout << "klavyeden girdiginiz deger: " << a << endl;
+    cout << "klavyeden girilen degerin 30 fazlasi: " << a + 30 << endl;
+    cout << "klavyeden girilen degerin 30 eksigi: " << a - 30 << endl;
+    
+    return 0;
+}
