29aba3168ac68f0ca55f385aca391dc412e96240
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-18 21:03:40 +0300

    Create icicedonguler.cpp

---
+++ icicedonguler.cpp
@@ -0,0 +1,21 @@
+//İÇ İÇE DÖNGÜLER
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    for (int x=0; x<10;x++) {
+            for (int y=0;y<10;y++){
+                    for (int z=0;z<10;z++){
+                
+                cout << x << " " << y << " " << z << " , " ;
+                
+                    }
+            }
+                cout<< endl;
+    }
+        
+    return 0;
+    
+}
