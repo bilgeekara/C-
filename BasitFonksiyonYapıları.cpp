c1dd7b097caf47e760ae238557fe0382f29dffab
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-22 00:22:13 +0300

    Create BasitFonksiyonYapıları.cpp

---
+++ BasitFonksiyonYapıları.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// f(x) = x + 10
+// f(20) = 20 + 10
+
+int f ( int x) {
+    
+    cout << x << endl;
+}
+    
+
+int main()
+{
+    f(5);
+    
+    
+    f(15);
+
+    
+}
