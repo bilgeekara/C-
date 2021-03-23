5c015112ecfbeeacd9f3562128d57f9d29b2ce43
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-23 18:37:30 +0300

    Create CokBoyutluDiziler.cpp

---
+++ CokBoyutluDiziler.cpp
@@ -0,0 +1,21 @@
+// ÇOK BOYUTLU DİZİLER          
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a[2][2] = {{5,10},{15,20}};
+    for (int i=0; i<2; i++){
+        for ( int j=0; j<2; j++ ){
+            
+            cout << a[i][j] << " " ;
+            
+            }
+            cout << endl; 
+        
+    }
+     
+    
+    
+}
