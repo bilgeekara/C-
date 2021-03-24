a391da71ddd1d7a8f7c8d3ed8a551cc3eded894e
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-03-24 21:36:16 +0300

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,17 @@
+// POINTERS(GÖSTERİCİLER)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=5;
+    int *p; 
+    p=&a;
+    
+    cout << "a: " << a << endl;
+    cout << "p: " << p << endl;
+    cout << "*p: " << *p << endl;
+    cout << "&a: " << &a << endl;
+    cout << "&p: " << &p << endl;
+      
+}
