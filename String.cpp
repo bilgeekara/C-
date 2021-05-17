13460f749f5cb0f9cc55c68702d019fa4a29c4c1
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-05-17 22:28:31 +0300

    Create String.cpp

---
+++ String.cpp
@@ -0,0 +1,27 @@
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    char *a= "kozmos";
+    cout << a << endl;
+    cout << a[3] << endl;
+    
+    char b[8]= "kodlama";
+    cout << b << endl;
+    
+
+}
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char x[4];
+    x[0]='k';
+    x[1]='o';
+    x[2]='d';
+    cout << x << endl;
+
+}
